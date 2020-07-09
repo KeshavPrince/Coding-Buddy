a3ea16ee1f803428d0103b1ee768bd5ad9998df3
KeshavPrince <keshavprince30@gmail.com>
2020-07-09 12:35:00 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {useNewUrlParser : true, useCreateIndex :true, useUnifiedTopology : true});
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('Database Attached..');
+}).then(() => {
+    console.log('Database Attached..');
+}).catch(() => {
+    console.log('Database error');
+});
+
+
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log('Wolf Listening to You..');
+});
